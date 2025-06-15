b46d0da201d469bfae15acdaf0bc873288c34b56
hakszi <me@hx4.me>
2025-06-15 15:25:40 +0200

    final

--- networth.py
+++ networth.py
@@ -4,26 +4,23 @@ import pandas as pd
 from matplotlib.ticker import FuncFormatter, StrMethodFormatter, NullFormatter, ScalarFormatter 
 from matplotlib import ticker
 
-# Adatok beolvasása (Month / Value oszlopok)
 data = pd.read_csv("data.csv")
-x = data['Month']
-y = data['Value']
+month = data['Month']
+value = data['Value']
 
-# Diagram készítése
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(10, 10), dpi=500)
 
-# Beállítások
 ax.tick_params(axis='x', rotation=-45) # Jobbra eldöntött x tengely jelölések
-plt.tight_layout() # szélesebb margó
-ax.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: f"{int(x):,}".replace(",", "'"))) # y tengely formatálás (' elválasztó)
+ax.yaxis.set_major_formatter(FuncFormatter(lambda value, pos: f"{int(value):,}".replace(",", "'"))) # y tengely formatálás (' elválasztó)
 
 # halvány vonal az y tengelyről könnyebb olvashatóságért
-ax.yaxis.grid(True, which='major', color='gray', linestyle='-', linewidth=0.5, alpha=0.3)
+ax.yaxis.grid(True, which='major', color='gray', linestyle='-', linewidth=0.5, alpha=0.5)
 ax.set_axisbelow(True)
 
 # Formátum (bar / line)
-ax.bar(x, y,color='red') # bar
-#ax.plot(x,y,color='red', marker='o') # line
+ax.bar(month, value,color='red',) # bar
+#ax.plot(month,value,color='red', marker='o') # line
 
 # Kiiratás
-plt.show()
+#plt.show()
+plt.savefig('output.png', dpi=500)  # save the heatmap as pdf for best quality
